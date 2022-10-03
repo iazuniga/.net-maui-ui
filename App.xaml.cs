0d96cd904f8e603e64dc98ac9e2e3008ec1c7367
iazuniga <114851104+iazuniga@users.noreply.github.com>
2022-10-03 15:45:01 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace PagesDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        
+		var navPage = new NavigationPage(new MainPage());
+
+		navPage.BarBackground = Colors.Chocolate;
+        navPage.BarTextColor = Colors.White;
+        //MainPage = navPage;
+
+         MainPage = new TabbedPageDemo();
+        // MainPage = new MainPage();
+        // MainPage = new ContentPageDemo();
+        // MainPage = new NavigationPage(new MainPage());
+        //MainPage = new FlyoutPageDemo();
+    }
+}
